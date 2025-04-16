8a94479b290b20f5ea888144c5f133027afad594
Teepat17 <asiaasdf719@gmail.com>
2025-04-16 19:26:27 +0700

    fix all problem

---
+++ next.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
+  },
+  images: {
+    domains: ['localhost'],
+  },
+};
+
+module.exports = nextConfig; 
